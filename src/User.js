11289b48f44cd8d2bb7c9aef88afda80a4457727
jeo19 <terrakkk@outlook.com>
2021-08-23 11:22:07 +0800

    feat:Create a User component for fetching the user's info

---
+++ src/User.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function User() {
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        setError(null);
+        setUsers(null);
+        setLoading(true);
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(response.data);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+    fetchUsers();
+  }, []);
+
+  return <div>Hello React!</div>;
+}
+
+export default User;
