75aa4aa18b1695904417c23546fc31f8c4d18ef7
jeo19 <terrakkk@outlook.com>
2021-08-31 09:32:19 +0800

    feat:Rendering user component

--- src/User.js
+++ src/User.js
@@ -2,16 +2,16 @@ import React from "react";
 import axios from "axios";
 import useAsync from "./useAsync";
 
-async function getUsers(id) {
+async function getUser(id) {
   const response = await axios.get(
     `https://jsonplaceholder.typicode.com/users/${id}`
   );
   return response.data;
 }
 function User({ id }) {
-  const [state] = useAsync(getUsers(id), [id]);
-
+  const [state] = useAsync(() => getUser(id), [id]);
   const { loading, data: user, error } = state;
+
   if (loading) return <div>Loading...</div>;
   if (error) return <div>occurring error!</div>;
   if (!user) return null;
--- src/Users.js
+++ src/Users.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import useAsync from "./useAsync";
+import User from "./User";
+
+async function getUsers() {
+  const response = await axios.get(
+    `https://jsonplaceholder.typicode.com/users`
+  );
+  return response.data;
+}
 
 function Users() {
-  return <div>Hello React!</div>;
+  const [userId, setUserId] = useState(null);
+  const [state, refetch] = useAsync(getUsers, [], true);
+  const { loading, data: users, error } = state;
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Occuring the error</div>;
+  if (!users) return <button onClick={refetch}>Import data</button>;
+  return (
+    <>
+      <ul>
+        {users.map((user) => (
+          <li
+            key={user.id}
+            onClick={() => setUserId(user.id)}
+            style={{ cursor: "pointer" }}
+          >
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={refetch}>Import again</button>
+      {userId && <User id={userId} />}
+    </>
+  );
 }
 
 export default Users;
