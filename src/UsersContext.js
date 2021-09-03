e83b22b22210b19e007653236be05df15c765c3e
jeo19 <terrakkk@outlook.com>
2021-09-03 08:17:53 +0800

    feat:Implement reducer for userContext API step02

--- src/UsersContext.js
+++ src/UsersContext.js
@@ -38,6 +38,31 @@ function usersReducer(state, action) {
         ...state,
         users: loadingState,
       };
+    case "GET_USERS_SUCCESS":
+      return {
+        ...state,
+        users: success(action.data),
+      };
+    case "GET_USERS_ERROR":
+      return {
+        ...state,
+        users: error(action.error),
+      };
+    case "GET_USER":
+      return {
+        ...state,
+        user: loadingState,
+      };
+    case "GET_USER_SUCCESS":
+      return {
+        ...state,
+        user: success(action.data),
+      };
+    case "GET_USER_ERROR":
+      return {
+        ...state,
+        user: error(action.error),
+      };
     default:
       throw new Error(`Unhandled action type:${action.type}`);
   }
