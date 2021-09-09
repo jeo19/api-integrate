3fb469a7854f02b06ce3725218998e29306c2bf9
jeo19 <terrakkk@outlook.com>
2021-09-09 17:54:20 +0800

    refact:Refactored the reducer function step02

--- src/UsersContext.js
+++ src/UsersContext.js
@@ -1,56 +1,15 @@
 import React, { createContext, useContext, useReducer } from "react";
-import { createAsyncDispatcher } from "./asyncActionUtils";
+import {
+  createAsyncDispatcher,
+  createAsyncHandler,
+  initialAsyncState,
+} from "./asyncActionUtils";
 import * as api from "./api";
 
 const initialState = {
-  users: {
-    loading: false,
-    data: null,
-    error: null,
-  },
-  user: {
-    loading: false,
-    data: null,
-    error: null,
-  },
+  users: initialAsyncState,
+  user: initialAsyncState,
 };
-
-function usersReducer(state, action) {
-  switch (action.type) {
-    case "GET_USERS":
-      return {
-        ...state,
-        users: loadingState,
-      };
-    case "GET_USERS_SUCCESS":
-      return {
-        ...state,
-        users: success(action.data),
-      };
-    case "GET_USERS_ERROR":
-      return {
-        ...state,
-        users: error(action.error),
-      };
-    case "GET_USER":
-      return {
-        ...state,
-        user: loadingState,
-      };
-    case "GET_USER_SUCCESS":
-      return {
-        ...state,
-        user: success(action.data),
-      };
-    case "GET_USER_ERROR":
-      return {
-        ...state,
-        user: error(action.error),
-      };
-    default:
-      throw new Error(`Unhandled action type:${action.type}`);
-  }
-}
 
 const UsersStateContext = createContext(null);
 const UsersDispatchContext = createContext(null);
--- src/asyncActionUtils.js
+++ src/asyncActionUtils.js
@@ -64,4 +64,5 @@ export function createAsyncHandler(type, key) {
         return state;
     }
   }
+  return handler;
 }
