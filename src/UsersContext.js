70becf1704f567c31087a0d8f6e3b3750b069802
jeo19 <terrakkk@outlook.com>
2021-09-09 18:12:05 +0800

    refact:Refactored the reducer function step03

--- src/UsersContext.js
+++ src/UsersContext.js
@@ -11,8 +11,25 @@ const initialState = {
   user: initialAsyncState,
 };
 
+const usersHandler = createAsyncHandler("GET_USERS", "users");
+const userHandler = createAsyncHandler("GET_USER", "user");
 const UsersStateContext = createContext(null);
 const UsersDispatchContext = createContext(null);
+
+function usersReducer(state, action) {
+  switch (action.type) {
+    case "GET_USERS":
+    case "GET_USERS_SUCCESS":
+    case "GET_USERS_ERROR":
+      return usersHandler(state, action);
+    case "GET_USER":
+    case "GET_USER_SUCCESS":
+    case "GET_USER_ERROR":
+      return userHandler(state, action);
+    default:
+      throw new Error(`Unhandled action type:${action.type}`);
+  }
+}
 
 //Wrapping the provider tag for use the state and dispatch values with global
 
